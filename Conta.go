8d95a2f6897ef86e9292c4f5297e6311e9065179
rvfmeli <rafael.vicente@mercadolivre.com>
2021-12-22 17:59:06 -0300

    : sparkles: Add Method Sacar

--- Conta.go
+++ Conta.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 import (
 	"strconv"
 )
@@ -8,31 +10,54 @@ import (
 type Conta struct {
 	titular string
 	numAgencia int
-	numConta string
+	numConta int
 	saldo float64
+
 }
 
+var valorConta float64 = 0
 
 
-func (c *Conta) Depositar(valorDoDeposito float64) (string, float64) {
-	if valorDoDeposito > 0 {
+
+func (c *Conta) Depositar(valorDoDeposito float64) (string, float64, string, float64) {
+	if valorDoDeposito >= valorConta {
 		c.saldo += valorDoDeposito
-		return "Depositado", c.saldo
+		return "Saldo agora é", c.saldo, "Valor depositado foi de: ", valorDoDeposito
 	} else {
-		return  "Valor insuficiente", c.saldo
+		return  "Valor insuficiente", c.saldo, "Insucesso", valorDoDeposito
+	}
+}
+
+func (c *Conta) Sacar (valorDoSaque float64) (string, float64, string, float64) {
+	if valorDoSaque >= valorConta {
+		c.saldo =- valorDoSaque
+		return "Seu saldo agora é: ", c.saldo, "Valor do saque foi de: ", valorDoSaque
+	} else {
+		return "Valor insuficiente: ", c.saldo, "Valor na conta é: ", valorDoSaque
 	}
 }
 
 
 
-func main ()  {
-	contaRafa := Conta{titular: "Rafa", numAgencia: 001, numConta: "100", saldo: 1000}
-	contaJully := Conta{titular: "July", numAgencia: 001, numConta: "100", saldo: 1000}
-	var saldoTeste int
 
-	fmt.Println( "Titular: " + contaRafa.titular + "\nNumero da Conta: " + contaRafa.numConta)
-	fmt.Println( "Titular: " + contaJully.titular + "Saldo: " + strconv.Itoa(saldoTeste))
-	status, valor := contaJully.Depositar(2000)
-	fmt.Println(status, valor)
+
+func main ()  {
+	contaRafa := Conta{titular: "Rafa", numAgencia: 001, numConta: 100, saldo: 0}
+	//contaJoiceChata := Conta{titular: "Joice", numAgencia: 001, numConta: 101, saldo: 10}
+	contaJully := Conta{titular: "July", numAgencia: 001, numConta: 102, saldo: 0}
+	//contaKapih := Conta{titular: "Kapih", numAgencia: 001, numConta: 103, saldo: 1000}
+
+	fmt.Println( "Titular: " + contaRafa.titular + "\nSaldo: " + strconv.FormatFloat(float64(contaRafa.saldo), 'f', 1,32))
+	saldoAtual, valor, status, valorDoDeposito  := contaRafa.Depositar(100 )
+	fmt.Println(saldoAtual, valor, status, valorDoDeposito)
+
+
+	fmt.Println( "Titular: " + contaJully.titular + "\nSaldo: " + strconv.FormatFloat(float64(contaJully.saldo), 'f', 1,32))
+	saldoAtual, valor, status, valorDoDeposito  = contaJully.Depositar(1500 )
+	fmt.Println(saldoAtual, valor, status, valorDoDeposito)
+
+	fmt.Println( "Titular: " + contaJully.titular + "\nSaldo: " + strconv.FormatFloat(float64(contaJully.saldo), 'f', 1,32))
+	saldoAtual, valor, status, valorDoSaque := contaJully.Sacar(300 )
+	fmt.Println(saldoAtual, valor, status, valorDoSaque)
 }
 
